d68deb7e538bee3b0ebe0c1f2813927866e95256
justin-joyce <justinjoyce88@gmail.com>
2018-10-22 12:00:09 -0400

    routing with emits

--- src/app/recipebook/recipe-list/recipe-list.component.ts
+++ src/app/recipebook/recipe-list/recipe-list.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  @Output()
+  recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'this is a test', 'https://www.maxpixel.net/static/photo/1x/Cooking-Delicious-Korean-Food-Recipe-Asia-822090.jpg')
+    new Recipe(
+      "A Test Recipe",
+      "this is a test",
+      "https://www.maxpixel.net/static/photo/1x/Cooking-Delicious-Korean-Food-Recipe-Asia-822090.jpg"
+    ),
+    new Recipe(
+      "Another Recipe",
+      "this is a test",
+      "https://www.maxpixel.net/static/photo/1x/Cooking-Delicious-Korean-Food-Recipe-Asia-822090.jpg"
+    )
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onRecipeSelect(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
-
 }
--- src/app/recipebook/recipebook.component.ts
+++ src/app/recipebook/recipebook.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipebook',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipebook.component.css']
 })
 export class RecipebookComponent implements OnInit {
+  selectedRecipe: Recipe;
 
   constructor() { }
 
