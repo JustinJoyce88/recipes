eb75b63f1cb9e89d41086ea7e0ed98e6b152c024
justin-joyce <justinjoyce88@gmail.com>
2018-11-26 10:09:33 -0500

    implement observable

--- src/app/recipebook/recipebook.component.ts
+++ src/app/recipebook/recipebook.component.ts
@@ -9,16 +9,10 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipebookComponent implements OnInit {
-  selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    )
   }
 
 }
--- src/app/shoppinglist/shoppinglist.component.ts
+++ src/app/shoppinglist/shoppinglist.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "./shoppinglist.service";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: "app-shoppinglist",
   templateUrl: "./shoppinglist.component.html",
   styleUrls: ["./shoppinglist.component.css"]
 })
-export class ShoppinglistComponent implements OnInit {
+export class ShoppinglistComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe(
+    this.subscription = this.shoppingListService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
-    )
+    );
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
